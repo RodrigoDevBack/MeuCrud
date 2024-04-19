34c0d2a6078f9af9523ecc4fc7a10aea79f417dc
RodrigoDevBack <155842654+RodrigoDevBack@users.noreply.github.com>
2024-04-18 23:31:15 -0300

    Meu primeiro CRUD

---
+++ interface/app.py
@@ -0,0 +1,66 @@
+import PySimpleGUI as sg
+from main.main import *
+sg.theme('DarkBlue6')
+
+layout = [[sg.InputText(key='Texto', do_not_clear=False, expand_x=True)],
+
+          [sg.Push(), sg.Button('Solicitar', expand_x=True), sg.Push(),
+           sg.Checkbox('Criar arquivo?'), sg.Push()],
+
+          [sg.Text(f'Arquivo atual: {nome_arquivo()}', key='arquivo')],
+
+          [sg.Text('', key='arq')],
+
+          [sg.Button('Mostrar dados atuais no arquivo', key='mostrar')],
+
+          [sg.Push(), sg.Text('', key='analise'), sg.Push()],
+
+          [sg.Text('Digite o índicie a ser removido:'),
+           sg.Input('', key='indicie', do_not_clear=False, size=20, expand_x=True),
+           sg.Button('Remover', key='remove', size=10),
+           sg.Button('Reescrever', key='reescrever', size=10)],
+
+          [sg.Text('', key='apagado', expand_x=True)]
+          ]
+
+window = sg.Window('Salvando informações', layout, margins=(50, 50), resizable=True, font=('Miriam Libre', 12),
+                   titlebar_font=('Miriam Libre', 10), element_justification='Center',
+                   use_ttk_buttons=True, auto_size_buttons=True, auto_size_text=True,
+                   )
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED:
+        break
+    if event == 'Solicitar':
+        if values[0]:
+            criar_arq(values['Texto'])
+            guardar_arquivo(values['Texto'])
+            window['arquivo'].update(f'Arquivo atual:{nome_arquivo()}')
+            window['arq'].update(f'Arquivo {values['Texto']} foi criado com êxito!!!')
+        else:
+            try:
+                escrever(values['Texto'])
+                window['arq'].update(f'As palavras {values['Texto']} foram escritas com êxito.')
+            except:
+                window['arq'].update('Arquivo não encontrado ou não criado. Por favor crie um arquivo novo.')
+    if event == 'mostrar':
+        window['analise'].update(mostrar_arq())
+    if event == 'remove':
+        try:
+            indice = int(values['indicie'])
+            apagado = apagar(indice)
+            window['apagado'].update(f'Palavra(as) apagadas: {apagado}')
+        except:
+            window['analise'].update(mostrar_arq())
+            num_indicie = values['indicie']
+            num_indicie = ''.join(num_indicie)
+            if len(num_indicie) > 0:
+                window['apagado'].update('O índicie descrito não existe.')
+        window['analise'].update(mostrar_arq())
+    if event == 'reescrever':
+        retornada = guardar_apagado(reescrever=True)
+        window['analise'].update(mostrar_arq())
+        window['apagado'].update(f'A(s) palavra(s) "{retornada}" foi reescrito no arquivo atual.')
+
+window.close()
---
+++ main/main.py
@@ -0,0 +1,103 @@
+def guardar_arquivo(nome): #Guarda o nome do último arquivo criado
+    arq = open('GuardarArquivo.txt', 'w')
+    arq.write(nome)
+    return nome
+
+
+def escrever(txt=''): #Escreve o que desejar no último arquivo criado
+    arq = open(f'{nome_arquivo()}', 'r')
+    analise = arq.readlines()
+    analise = len(analise)
+    arq = open(f'{nome_arquivo()}', 'a+t')
+    if analise > 0:
+        arq.write(f'\n{txt}')
+    else:
+        arq.write(txt)
+
+
+def criar_arq(name): #Cria o arquivo com o nome escolhido
+    arq = open(f'{name}.txt', 'w')
+    guardar_arquivo(name)
+    return True
+
+
+def nome_arquivo():#Pega o nome do último arquivo criado
+    try:
+        arq = open('GuardarArquivo.txt', 'r+t')
+        dados = arq.readlines()
+        dados = ''.join(dados)
+        try:
+            dados = dados.replace('\n', '')
+        except:
+            dados = dados
+        return F'{dados}.txt'
+    except:
+        arq = open('GuardarArquivo.txt', 'w+')
+        dados = arq.readlines()
+        dados = ''.join(dados)
+        try:
+            dados = dados.replace('\n', '')
+        except:
+            dados = dados
+        return F'{dados}.txt'
+
+
+def mostrar_arq(): #Retorna os itens de dentro do último arquivo criado.
+    try:
+        arq = open(f'{nome_arquivo()}', 'r+')
+        dados = arq.readlines()
+        dados = ''.join(dados)
+        return dados
+    except:
+        return 'Arquivo não encontrado'
+
+
+def apagar(indicie): #Apaga um item no índicie escolhido
+    arq = open(f'{nome_arquivo()}', 'r')
+    info = arq.readlines()
+    palavra_apagar = info.pop(indicie)
+    guardar_apagado(palavra_apagar)
+    arq.close()
+    arq = open(f'{nome_arquivo()}', 'w+')
+    for i in info:
+        arq.write(i)
+    return palavra_apagar
+
+
+def guardar_apagado(item=None, reescrever=False): # Guarda as palavras apagadas e retorna as palavras apagadas quando desejado
+    if reescrever is False:
+        try:
+            arq = open('reescrever.txt', 'r+')
+        except:
+            arq = open('reescrever.txt', 'w+')
+            arq.close()
+            arq = open('reescrever.txt', 'r+')
+        analise = arq.readlines()
+        analise = len(analise)
+        if analise > 0:
+            arq.write(f'\n{item}')
+        else:
+            arq.write(item)
+        return None
+    else:
+        try:
+            arq = open('reescrever.txt', 'r+')
+        except:
+            arq = open('reescrever.txt', 'w+')
+            arq.close()
+            arq = open('reescrever.txt', 'r+')
+        analise = arq.readlines()
+
+        try:
+            palavra = analise.pop()
+            arq.close()
+            arq = open('reescrever.txt', 'w+')
+            for i in analise:
+                i = i.replace('\n', '')
+                arq.write(i)
+            arq.close()
+            palavra = palavra.replace('\n', '')
+            escrever(palavra)
+            return palavra
+        except:
+            return None
