622e0446a98a681e727e64653c1f2132cd56284c
RodrigoDevBack <155842654+RodrigoDevBack@users.noreply.github.com>
2024-04-18 23:37:22 -0300

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,10 +1,10 @@
-def guardar_arquivo(nome): #Guarda o nome do último arquivo criado
+def guardar_arquivo(nome): #Guarda o nome do último arquivo .txt criado
     arq = open('GuardarArquivo.txt', 'w')
     arq.write(nome)
     return nome
 
 
-def escrever(txt=''): #Escreve o que desejar no último arquivo criado
+def escrever(txt=''): #Escreve o que desejar no último arquivo .txt criado
     arq = open(f'{nome_arquivo()}', 'r')
     analise = arq.readlines()
     analise = len(analise)
@@ -15,13 +15,13 @@ def escrever(txt=''): #Escreve o que desejar no último arquivo criado
         arq.write(txt)
 
 
-def criar_arq(name): #Cria o arquivo com o nome escolhido
+def criar_arq(name): #Cria o arquivo com o nome .txt escolhido
     arq = open(f'{name}.txt', 'w')
     guardar_arquivo(name)
     return True
 
 
-def nome_arquivo():#Pega o nome do último arquivo criado
+def nome_arquivo():#Pega o nome do último arquivo .txt criado
     try:
         arq = open('GuardarArquivo.txt', 'r+t')
         dados = arq.readlines()
@@ -42,7 +42,7 @@ def nome_arquivo():#Pega o nome do último arquivo criado
         return F'{dados}.txt'
 
 
-def mostrar_arq(): #Retorna os itens de dentro do último arquivo criado.
+def mostrar_arq(): #Retorna os itens de dentro do último arquivo .txt criado.
     try:
         arq = open(f'{nome_arquivo()}', 'r+')
         dados = arq.readlines()
@@ -64,7 +64,7 @@ def apagar(indicie): #Apaga um item no índicie escolhido
     return palavra_apagar
 
 
-def guardar_apagado(item=None, reescrever=False): # Guarda as palavras apagadas e retorna as palavras apagadas quando desejado
+def guardar_apagado(item=None, reescrever=False): # Guarda as palavras apagadas e reescreve-as quando desejado enquanto a janela não for fechada.
     if reescrever is False:
         try:
             arq = open('reescrever.txt', 'r+')
