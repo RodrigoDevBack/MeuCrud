7d2a915d0911c8a6baeaa5e16e12dd80aa02cbae
RodrigoDevBack <155842654+RodrigoDevBack@users.noreply.github.com>
2024-04-19 21:46:06 -0300

    Problema de utilização
    Possível solução criada.

--- app/window.py
+++ app/window.py
@@ -1,5 +1,7 @@
 import PySimpleGUI as sg
-from main.main import *
+
+import main
+
 sg.theme('DarkBlue6')
 
 layout = [[sg.InputText(key='Texto', do_not_clear=False, expand_x=True)],
@@ -7,7 +9,7 @@ layout = [[sg.InputText(key='Texto', do_not_clear=False, expand_x=True)],
           [sg.Push(), sg.Button('Solicitar', expand_x=True), sg.Push(),
            sg.Checkbox('Criar arquivo?'), sg.Push()],
 
-          [sg.Text(f'Arquivo atual: {nome_arquivo()}', key='arquivo')],
+          [sg.Text(f'Arquivo atual: {main.nome_arquivo()}', key='arquivo')],
 
           [sg.Text('', key='arq')],
 
@@ -34,33 +36,33 @@ while True:
         break
     if event == 'Solicitar':
         if values[0]:
-            criar_arq(values['Texto'])
-            guardar_arquivo(values['Texto'])
-            window['arquivo'].update(f'Arquivo atual:{nome_arquivo()}')
+            main.criar_arq(values['Texto'])
+            main.guardar_arquivo(values['Texto'])
+            window['arquivo'].update(f'Arquivo atual:{main.nome_arquivo()}')
             window['arq'].update(f'Arquivo {values['Texto']} foi criado com êxito!!!')
         else:
             try:
-                escrever(values['Texto'])
+                main.escrever(values['Texto'])
                 window['arq'].update(f'As palavras {values['Texto']} foram escritas com êxito.')
             except:
                 window['arq'].update('Arquivo não encontrado ou não criado. Por favor crie um arquivo novo.')
     if event == 'mostrar':
-        window['analise'].update(mostrar_arq())
+        window['analise'].update(main.mostrar_arq())
     if event == 'remove':
         try:
             indice = int(values['indicie'])
-            apagado = apagar(indice)
+            apagado = main.apagar(indice)
             window['apagado'].update(f'Palavra(as) apagadas: {apagado}')
         except:
-            window['analise'].update(mostrar_arq())
+            window['analise'].update(main.mostrar_arq())
             num_indicie = values['indicie']
             num_indicie = ''.join(num_indicie)
             if len(num_indicie) > 0:
                 window['apagado'].update('O índicie descrito não existe.')
-        window['analise'].update(mostrar_arq())
+        window['analise'].update(main.mostrar_arq())
     if event == 'reescrever':
-        retornada = guardar_apagado(reescrever=True)
-        window['analise'].update(mostrar_arq())
+        retornada = main.guardar_apagado(reescrever=True)
+        window['analise'].update(main.mostrar_arq())
         window['apagado'].update(f'A(s) palavra(s) "{retornada}" foi reescrito no arquivo atual.')
 
 window.close()
